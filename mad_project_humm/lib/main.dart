806cd83b5e1a6234398e500ef2577d63e0ab2ca3
yunjaedo <two.minuto@hotmail.com>
2019-11-11 10:58:34 +0900

    loading page added

--- mad_project_humm/lib/main.dart
+++ mad_project_humm/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'loading.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,6 +22,10 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: './loading',
+      routes: {
+        './loading': (context) => LoadingScreen(),
+      },
     );
   }
 }
