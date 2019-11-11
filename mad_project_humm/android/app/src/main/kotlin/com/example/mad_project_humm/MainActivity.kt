a4223d5a314727fce37314080fb371ef9660920c
yunjaedo <two.minuto@hotmail.com>
2019-11-11 10:26:09 +0900

    new project

---
+++ mad_project_humm/android/app/src/main/kotlin/com/example/mad_project_humm/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mad_project_humm
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
