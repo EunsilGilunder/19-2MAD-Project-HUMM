ee58c50ab6b4d2a4e8e360af381de952083617c6
yunjaedo <two.minuto@hotmail.com>
2019-11-11 11:13:40 +0900

    dislikes* circular progress indicator

--- mad_project_humm/lib/loading.dart
+++ mad_project_humm/lib/loading.dart
@@ -3,23 +3,31 @@ import "package:flutter/material.dart";
 class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Image(
-            image: NetworkImage(
-                'https://firebasestorage.googleapis.com/v0/b/project-humm.appspot.com/o/Image%2Flogo%2FHUMM_logo.png?alt=media&token=8e92cbfb-926d-4af1-9965-c5d7914e64b9'),
-          ),
-          SizedBox(
-            height: 100,
-          ),
-          CircularProgressIndicator(
-            strokeWidth: 8,
-            backgroundColor: Colors.blueGrey,
-          ),
-        ],
+    return Container(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 250,
+              child: Image(
+                image: NetworkImage(
+                    'https://firebasestorage.googleapis.com/v0/b/project-humm.appspot.com/o/Image%2Flogo%2FHUMM_logo.png?alt=media&token=8e92cbfb-926d-4af1-9965-c5d7914e64b9'),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            SizedBox(
+              width: 30,
+              height: 30,
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.blueGrey,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
