bc69abcd7a8198b66eb392ca8e76b362a8e00e30
MinnJun <56334239+MinnJun@users.noreply.github.com>
2019-11-28 03:51:23 +0900

    home page and profile page commit

--- 19-2MAD-Project-HUMM-master/mad_project_humm/lib/home.dart
+++ 19-2MAD-Project-HUMM-master/mad_project_humm/lib/home.dart
@@ -1,24 +1,243 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_calendar/flutter_clean_calendar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class Main extends StatelessWidget {
+
+class Main extends StatefulWidget {
+  @override
+  State<StatefulWidget> createStaete(){
+    return _MainState();
+  }
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+
+  void _handleNewDate(date) {
+    setState(() {
+      _selectedDay = date;
+      _selectedEvents = _events[_selectedDay] ?? [];
+    });
+    print(_selectedEvents);
+  }
+
+  List _selectedEvents;
+  DateTime _selectedDay;
+
+  final Map _events = {
+    DateTime(2019, 11, 31): [
+      {'name': 'Event A', 'isDone': true},
+    ],
+    DateTime(2019, 12, 1): [
+      {'name': 'Event A', 'isDone': true},
+      {'name': 'Event B', 'isDone': true},
+    ],
+    DateTime(2019, 12, 8): [
+      {'name': 'Event A', 'isDone': true},
+      {'name': 'Event B', 'isDone': true},
+    ],
+    DateTime(2019, 12, 4): [
+      {'name': 'Event A', 'isDone': true},
+      {'name': 'Event B', 'isDone': true},
+      {'name': 'Event C', 'isDone': false},
+    ],
+    DateTime(2019, 12, 25): [
+      {'name': 'Event A', 'isDone': true},
+      {'name': 'Event B', 'isDone': true},
+      {'name': 'Event C', 'isDone': false},
+    ],
+    DateTime(2019, 11, 30): [
+      {'name': 'Event A', 'isDone': false},
+    ],
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedEvents = _events[_selectedDay] ?? [];
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar : AppBar(
-        leading : IconButton(
-          icon : Icon(Icons.menu),
-          onPressed:
-          (){}
-        ),
-        title : Image.network(
-          "https://firebasestorage.googleapis.com/v0/b/project-humm.appspot.com/o/Image%2Flogo%2FHUMM_logo.png?alt=media"
-        )
-
+      appBar: AppBar(
+        title : Text('Main Page'),
       ),
-      body:
-        ListView(
 
+      drawer : Drawer(
+        child : ListView(
+          padding : EdgeInsets.zero,
+          children: <Widget>[
+            Container(
+
+              child: DrawerHeader(
+                padding: EdgeInsets.only(left:10),
+                child : GestureDetector(
+                  onTap:() {
+                    Navigator.pushNamed(context,'./mypage');
+                  },
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>
+                    [
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: CircleAvatar(
+
+                            backgroundImage:NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZFKB5YleBlsuHbYUTpLXgGzOWidgRx55ANwYn0F7-m5qUT7yT&s'),
+                          radius: 35,
+                        ),
+                      ),
+                   Column(
+                     mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding:EdgeInsets.fromLTRB(10,0,10,8),
+                          child: Text("@coding_jo_a",
+                              style:TextStyle(color : Colors.white,
+                                  fontWeight: FontWeight.bold,
+                              fontSize: 20)
+                          ),
+                        ),
+                        Padding(
+                          padding:EdgeInsets.fromLTRB(10,0,10,10),
+                            child: Text("박민준 학부생님, 환영합니다!",
+                          style:TextStyle(
+                          color : Colors.white,
+                        )
+                        ))
+                      ],
+                    ),
+                    ]
+                  ),
+                )
+              ),
+              color: Colors.blueAccent,
+            ),
+            ListTile(
+              title: Text('동아리 보기',
+              style:TextStyle(fontWeight:FontWeight.bold,
+              fontSize:15)),
+              onTap:(){
+                Navigator.pop(context);
+              }
+            ),
+            ListTile(
+                title: Text('체험하기',
+    style:TextStyle(fontWeight:FontWeight.bold,
+    fontSize:15)),
+                onTap:(){
+                  Navigator.pop(context);
+                }
+            ),
+            ListTile(
+                title: Text('함께하기',style:TextStyle(fontWeight:FontWeight.bold,
+    fontSize:15)),
+                onTap:(){
+                  Navigator.pop(context);
+                }
+            ),
+            ListTile(
+                title: Text('내 활동',style:TextStyle(fontWeight:FontWeight.bold,
+                    fontSize:15)),
+                onTap:(){
+                  Navigator.pop(context);
+                }
+            ),
+
+          ]
         )
+      ),
+
+
+      body: SafeArea(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding : EdgeInsets.all(10.0),
+                        child: Calendar(
+                            events: _events,
+                            onRangeSelected: (range) =>
+                                print("Range is ${range.from}, ${range.to}"),
+                            onDateSelected: (date) => _handleNewDate(date),
+                            isExpandable: true,
+                            showTodayIcon: true,
+                            eventDoneColor: Colors.green,
+                            eventColor: Colors.grey),
+                      ),
+                      _buildEventList(),
+                      Container(
+                        child:Text('모집중'),
+                      ),
+                      CarouselSlider(
+                        height:200,
+                        items:[1].map((i){
+                          return Builder(
+                              builder:(BuildContext context){
+                                return Container(
+                                    width:MediaQuery.of(context).size.width,
+                                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                                    child : Stack(
+                                        children:<Widget>[
+                                          Image.network('https://firebasestorage.googleapis.com/v0/b/project-humm.appspot.com/o/Image%2Flogo%2FHUMM_logo.png?alt=media&token=8e92cbfb-926d-4af1-9965-c5d7914e64b9'),
+                                          Text('동아리 배너')
+                                        ]
+
+                                    )
+                                );
+                              }
+                          );
+                        }).toList(),
+                      ),
+                      Container(
+                        child:Text('동아리 체험활동')
+                      ),
+                      CarouselSlider(
+                        height:200,
+                        items:[1].map((i){
+                          return Builder(
+                            builder:(BuildContext context){
+                              return Container(
+                                width:MediaQuery.of(context).size.width,
+                                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                                child : Stack(
+                                  children:<Widget>[
+                                    Image.network('https://firebasestorage.googleapis.com/v0/b/project-humm.appspot.com/o/Image%2Flogo%2FHUMM_logo.png?alt=media&token=8e92cbfb-926d-4af1-9965-c5d7914e64b9'),
+                                    Text('동아리 배너')
+                                  ]
+
+                                )
+                              );
+                            }
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
+                ),
+            );
+  }
+
+  Widget _buildEventList() {
+    return Expanded(
+      child: ListView.builder(
+        itemBuilder: (BuildContext context, int index) => Container(
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(width: 1.5, color: Colors.black12),
+            ),
+          ),
+          padding:
+          const EdgeInsets.symmetric(horizontal: 0.0, vertical: 4.0),
+          child: ListTile(
+            title: Text(_selectedEvents[index]['name'].toString()),
+            onTap: () {},
+          ),
+        ),
+        itemCount: _selectedEvents.length,
+      ),
     );
   }
 }
---
+++ 19-2MAD-Project-HUMM-master/mad_project_humm/lib/mypage.dart
@@ -0,0 +1,210 @@
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'src/circular_percent_indicator.dart';
+
+
+class DetailPage extends StatefulWidget {
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  File _image;
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar :AppBar(
+          backgroundColor: const Color(0xFFFFFFFF),
+
+      ),
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top:10),
+              child: Column(
+                mainAxisSize : MainAxisSize.max,
+                mainAxisAlignment : MainAxisAlignment.center,
+                children : <Widget>[
+                  Container(
+                    padding:EdgeInsets.only(top:10),
+                    child : Stack(
+                        children: <Widget>[
+                          CircularPercentIndicator(
+                            radius: 120,
+                            lineWidth : 7.0,
+                            percent : 0.7,
+                            center : CircleAvatar(
+                              backgroundImage : NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZFKB5YleBlsuHbYUTpLXgGzOWidgRx55ANwYn0F7-m5qUT7yT&s'),
+                              radius: 53,
+                            ),
+                            progressColor : Color(0xff00b3e3),
+                            circularStrokeCap: CircularStrokeCap.round,
+                            backgroundColor : Color(0xffc4c4c4),
+                          ),]),
+                  ),
+                  Container(
+                      padding:EdgeInsets.all(10),
+                      child:Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children:<Widget>[
+                            Text('level 1',
+                                style : TextStyle(
+                                    fontSize:17,
+                                    fontWeight:FontWeight.bold
+                                )
+                            ),
+                            Text('@coding_jo_a',
+                                style : TextStyle(
+                                    fontSize:13,
+                                    fontWeight:FontWeight.bold),
+
+                            )]
+                      )
+                  ),
+                  Row(
+                      children:<Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(3.0),
+                          child: Chip(
+
+                            backgroundColor: Colors.white,
+                            label : Text('#커스텀케이크',
+                              style : TextStyle(
+                                  fontSize : 15,
+                                  fontWeight:FontWeight.bold,
+                                  color : Color(0xFF00b3e3)
+                              ),
+                            ),
+                            shape : RoundedRectangleBorder(
+                                borderRadius:BorderRadius.circular(30.0),
+                                side:BorderSide(color:Color(0xFF00b3e3))
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(3.0),
+                          child: Chip(
+
+                            backgroundColor: Colors.white,
+                            label : Text('#홈베이킹',
+                              style : TextStyle(
+                                  fontSize : 15,
+                                  fontWeight:FontWeight.bold,
+                                  color : Color(0xFF00b3e3)
+                              ),
+                            ),
+                            shape : RoundedRectangleBorder(
+                                borderRadius:BorderRadius.circular(30.0),
+                                side:BorderSide(color:Color(0xFF00b3e3))
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(3.0),
+                          child: Chip(
+
+                            backgroundColor: Colors.white,
+                            label : Text('#디저트',
+                              style : TextStyle(
+                                  fontSize : 15,
+                                  fontWeight:FontWeight.bold,
+                                  color : Color(0xFF00b3e3)
+                              ),
+                            ),
+                            shape : RoundedRectangleBorder(
+                                borderRadius:BorderRadius.circular(30.0),
+                                side:BorderSide(color:Color(0xFF00b3e3))
+                            ),
+                          ),
+                        ),
+                      ]
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+
+            Divider(),
+            Row(
+                children : <Widget> [
+                  Container(
+                      padding : EdgeInsets.fromLTRB(0,10,15,0),
+                      child : Row(
+                          children: <Widget>[
+                            SizedBox(
+                              width : 200,
+                              height : 60,
+                              child:
+                              TextField(
+                                  controller: _usernameController,
+                                  decoration: InputDecoration.collapsed(
+                                    hintText : '제목 입력',
+                                    hintStyle:
+                                    TextStyle(
+                                      fontSize : 30,
+                                      fontWeight:FontWeight.bold,
+                                    ),
+                                  ),
+                                  textAlign:TextAlign.start,
+                                  style: TextStyle(
+                                    fontSize : 30,
+                                    fontWeight: FontWeight.bold,
+                                  )
+                              ),
+
+                            ),
+                          ]
+                      )
+                  )
+                ]
+            ),
+            Divider(),
+            Row(
+                children : <Widget> [
+                  Container(
+                      padding : EdgeInsets.fromLTRB(0,10,15,0),
+                      child : Row(
+                          children: <Widget>[
+                            SizedBox(
+                              width : 200,
+                              height :200,
+                              child:
+                              TextField(
+                                  controller: _passwordController,
+                                  decoration: InputDecoration.collapsed(
+                                    hintText : '내용 입력',
+                                    hintStyle:
+                                    TextStyle(
+                                      fontSize : 15,
+                                    ),
+                                  ),
+                                  textAlign:TextAlign.start,
+                                  style: TextStyle(
+                                    fontSize : 15,
+                                  )
+                              ),
+
+                            ),
+                          ]
+                      )
+                  )
+                ]
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// TODO: Add AccentColorOverride (103)
